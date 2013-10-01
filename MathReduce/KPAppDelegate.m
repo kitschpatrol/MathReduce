aa76d71a3c6c7cd34fdbea1ac6004513204cb650
kitschpatrol <ermika@gmail.com>
2013-10-01 19:57:26 -0400

    Clean up.

--- MathReduce/KPAppDelegate.m
+++ MathReduce/KPAppDelegate.m
@@ -14,72 +14,47 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
     [NSApp setServicesProvider:self];
 }
 
-
-- (NSString *)rotateLettersInString:(NSString *)aString {
-    NSString *newString;
-    NSUInteger length;
-    unichar *buf;
-    
-    length = [aString length];
-    buf = malloc( (length + 1) * sizeof(unichar) );
-    [aString getCharacters:buf];
-    buf[length] = (unichar)0; // not really needed....
-    for (NSUInteger i = 0; i < length; i++) {
-        if (buf[i] >= (unichar)'a' && buf[i] <= (unichar) 'z') {
-            buf[i] += 13;
-            if (buf[i] > 'z') buf[i] -= 26;
-        } else if (buf[i] >= (unichar)'A' && buf[i] <= (unichar) 'Z') {
-            buf[i] += 13;
-            if (buf[i] > 'Z') buf[i] -= 26;
-        }
-    }
-    newString = [NSString stringWithCharacters:buf length:length];
-    free(buf);
-    return newString;
-}
-
-- (void)simpleEncrypt:(NSPasteboard *)pboard
-             userData:(NSString *)userData error:(NSString **)error {
-    
+- (void)reduceMath:(NSPasteboard *)pboard
+             userData:(NSString *)userData
+                error:(NSString **)error
+{
     // Test for strings on the pasteboard.
-    NSArray *classes = [NSArray arrayWithObject:[NSString class]];
-    NSDictionary *options = [NSDictionary dictionary];
+    NSArray *classes = @[[NSString class]];
+    NSDictionary *options = @{};
     
     if (![pboard canReadObjectForClasses:classes options:options]) {
-
         *error = NSLocalizedString(@"Error: Invalid substrate for Math Reduce.", nil);
         return;
     }
-    
+
     // Get and encrypt the string.
     NSString *pboardString = [pboard stringForType:NSPasteboardTypeString];
     NSError *mathError = nil;
-    [DDParser setDefaultPowerAssociativity:DDOperatorAssociativityRight];
-    DDMathEvaluator *evaluator = [[DDMathEvaluator alloc] init];
-    DDMathStringTokenizer *tokenizer = [[DDMathStringTokenizer alloc] initWithString:pboardString error:&mathError];
-    DDParser *parser = [DDParser parserWithTokenizer:tokenizer error:&mathError];
-    DDExpression *expression = [parser parsedExpressionWithError:&mathError];
-    DDExpression *rewritten = [evaluator expressionByRewritingExpression:expression];
+    DDExpression *expression = [DDExpression expressionFromString:pboardString error:&mathError];
     
-    NSNumber *value = [rewritten evaluateWithSubstitutions:nil evaluator:evaluator error:&mathError];
-    DD_RELEASE(tokenizer);
-    
-    if (value == nil) {
-        *error = [NSString stringWithFormat:NSLocalizedString(@"Math Reduce Error:\n%@", nil), [[mathError description] UTF8String]];
+    if (mathError) {
+        *error = [mathError description];
         return;
-    } else {
-        NSString *result = [value description];
-        [pboard clearContents];
-        [pboard writeObjects:[NSArray arrayWithObject:result]];
     }
     
-    // Write the encrypted string onto the pasteboard.
-
-
+    NSNumber *reduced = [expression evaluateWithSubstitutions:nil evaluator:nil error:&mathError];
+    
+    if (mathError) {
+        *error = [mathError description];
+        return;
+    }
+    
+    if (reduced == nil) {
+        *error = @"Nil value";
+        return;
+    }
+    
+    // Good value, put it on the pasteboard
+    [pboard clearContents];
+    [pboard writeObjects:[NSArray arrayWithObject:[reduced description]]];
 }
 
 
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 MathReduce
 ==========
+It's a very thin wrapper around the excellent 
+
+
+
+Note that the <kbd>⌘</kbd>-<kbd>⇧</kbd>-<kbd>M</kbd> shortcut is in conflict with a default 
+
+Debug Workflow > View Memory (Debug Menu)
