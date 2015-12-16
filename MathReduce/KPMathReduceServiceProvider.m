133325a869881ab8adf9394f03849504f5f5aff9
Eric Mika <eric@ericmika.com>
2015-12-15 22:14:59 -0500

    Update DDMathParser to version 1.1.

--- MathReduce/KPMathReduceServiceProvider.m
+++ MathReduce/KPMathReduceServiceProvider.m
@@ -6,49 +6,47 @@
 //  Copyright (c) 2013 Eric Mika. All rights reserved.
 //
 
-#import "KPMathReduceServiceProvider.h"
 #import "DDExpression.h"
+#import "DDMathEvaluator.h"
+#import "KPMathReduceServiceProvider.h"
 
 @implementation KPMathReduceServiceProvider
 
-- (void)reduceMath:(NSPasteboard *)pboard
-          userData:(NSString *)userData
-             error:(NSString **)error
-{
-    // Test for strings on the pasteboard.
-    NSArray *classes = @[[NSString class]];
-    NSDictionary *options = @{};
-    
-    if (![pboard canReadObjectForClasses:classes options:options]) {
-        *error = NSLocalizedString(@"Invalid substrate for Math Reduce.", nil);
-        return;
-    }
-    
-    // Get and encrypt the string.
-    NSString *pboardString = [pboard stringForType:NSPasteboardTypeString];
-    NSError *mathError = nil;
-    DDExpression *expression = [DDExpression expressionFromString:pboardString error:&mathError];
-    
-    if (mathError) {
-        *error = [mathError description];
-        return;
-    }
-    
-    NSNumber *reduced = [expression evaluateWithSubstitutions:nil evaluator:nil error:&mathError];
-    
-    if (mathError) {
-        *error = [mathError description];
-        return;
-    }
-    
-    if (reduced == nil) {
-        *error = NSLocalizedString(@"Invalid substrate for Math Reduce.", nil);
-        return;
-    }
-    
-    // Good value, put it on the pasteboard
-    [pboard clearContents];
-    [pboard writeObjects:[NSArray arrayWithObject:[reduced description]]];
+- (void)reduceMath:(NSPasteboard *)pboard userData:(NSString *)userData error:(NSString **)error {
+	// Test for strings on the pasteboard.
+	NSArray *classes = @[[NSString class]];
+	NSDictionary *options = @{};
+
+	if (![pboard canReadObjectForClasses:classes options:options]) {
+		*error = NSLocalizedString(@"Invalid substrate for Math Reduce.", nil);
+		return;
+	}
+
+	// Get and encrypt the string.
+	NSString *pboardString = [pboard stringForType:NSPasteboardTypeString];
+	NSError *mathError = nil;
+	DDExpression *expression = [DDExpression expressionFromString:pboardString error:&mathError];
+
+	if (mathError) {
+		*error = [mathError description];
+		return;
+	}
+
+	NSNumber *reduced = [[DDMathEvaluator defaultMathEvaluator] evaluateExpression:expression withSubstitutions:nil error:&mathError];
+
+	if (mathError) {
+		*error = [mathError description];
+		return;
+	}
+
+	if (reduced == nil) {
+		*error = NSLocalizedString(@"Invalid substrate for Math Reduce.", nil);
+		return;
+	}
+
+	// Good value, put it on the pasteboard
+	[pboard clearContents];
+	[pboard writeObjects:[NSArray arrayWithObject:[reduced description]]];
 }
 
 @end
